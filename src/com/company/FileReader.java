9206ead3b3c33d400fdf47ba34180e2cf13ddb62
NamorV <drednout309@gmail.com>
2019-02-10 19:43:02 +0100

    refactoring

---
+++ src/com/company/FileReader.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class FileReader {
+    private File file;
+    private String extension;
+    private byte[] fileContent;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public byte[] getFileContent() {
+        return fileContent;
+    }
+
+    public void setFileContent(byte[] fileContent) {
+        this.fileContent = fileContent;
+    }
+
+    public void getFileByName(String fileName) throws Exception {
+        file = new File(fileName);
+
+        if(!file.exists()) {
+            throw new Exception("There is no such file!");
+        }
+
+        fileContent = Files.readAllBytes(file.toPath());
+        String name = file.getName();
+        extension = name.substring(name.lastIndexOf("."));
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,14 @@
 package com.company;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        File file = new File("tenor.txt");
-        byte[] fileContent = Files.readAllBytes(file.toPath());
+    public static void main(String[] args) throws Exception {
+        FileReader fileReader = new FileReader();
         Differentiator differentiator = new Differentiator();
-        String name = file.getName();
-        String extension = name.substring(name.lastIndexOf("."));
-        differentiator.validate(fileContent, extension);
+        fileReader.getFileByName("a.txt");
+
+        differentiator.validate(fileReader.getFileContent(), fileReader.getExtension());
     }
 }
