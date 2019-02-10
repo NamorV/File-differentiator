ebf4f9657272e84708a4cd54ee7dc2e22dae76c5
NamorV <drednout309@gmail.com>
2019-02-10 18:35:21 +0100

    file reader

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Main {
 
-    public static void main(String[] args) {
-     System.out.print("a");
+    public static void main(String[] args) throws IOException {
+        File file = new File("a.jpg");
+        System.out.println(file.toString());
+
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+        System.out.println(fileContent);
     }
 }
