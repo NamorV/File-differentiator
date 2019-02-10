074385f4a2aa40fa1415736ce9f10611735fc1ac
NamorV <drednout309@gmail.com>
2019-02-10 18:45:35 +0100

    jpg validator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,23 @@ package com.company;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
 
 public class Main {
+    private static final byte[] JPG_MAGIC_NUMBER = {(byte) 0xFF, (byte) 0xD8, (byte) 0xFF};
 
     public static void main(String[] args) throws IOException {
         File file = new File("a.jpg");
-        System.out.println(file.toString());
-
         byte[] fileContent = Files.readAllBytes(file.toPath());
-        System.out.println(fileContent);
+
+        byte[] actualMagicNumber = new byte[JPG_MAGIC_NUMBER.length];
+
+        for(int i = 0; i < JPG_MAGIC_NUMBER.length; i++) {
+            actualMagicNumber[i] = fileContent[i];
+        }
+
+        if(Arrays.equals(actualMagicNumber, JPG_MAGIC_NUMBER)) {
+            System.out.println("Its JPG!");
+        }
     }
 }
